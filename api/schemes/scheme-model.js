0861c57e40a49ad61fd4720ac7cc3103560e4893
Raj-04 <1204patel.raj@gmail.com>
2021-12-08 17:35:23 -0500

    findById model done

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,6 +1,6 @@
 const db = require('../../data/db-config')
 
-// function find() { // EXERCISE A
+// EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
@@ -26,7 +26,7 @@ function find() {
     .orderBy('sc.scheme_id', 'asc')
 }
 
-function findById(scheme_id) { // EXERCISE B
+ // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -92,6 +92,29 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+async function findById(scheme_id) {
+  const rows = await db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
+    .where('sc.scheme_id', scheme_id)
+    .orderBy('st.step_number', 'asc')
+  
+  const result = {
+    scheme_id: rows[0].scheme_id,
+    scheme_name: rows[0].scheme_name,
+    steps: []
+  }
+
+  rows.forEach(row => {
+    if (row.step_id) {
+      result.steps.push({
+        step_id: row.step_id,
+        step_number: row.step_number,
+        instructions: row.instructions
+      })
+    }
+  })
+  return result
 }
 
 function findSteps(scheme_id) { // EXERCISE C
